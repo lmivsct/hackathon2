fc03ff6b34509fa0beac5d8ba05fe6f0dbb14a74
Laurent Michelet <lmichelet@voyages-sncf.com>
2014-12-09 15:43:29 +0100

    with help page

--- src/main/java/com/vsct/hackathon/AutoCompleteResource.java
+++ src/main/java/com/vsct/hackathon/AutoCompleteResource.java
@@ -1,5 +1,7 @@
 package com.vsct.hackathon;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -22,7 +24,37 @@ public class AutoCompleteResource {
     public Response autocomplete(@QueryParam("query") String query) throws JSONException {
         final StringBuilder responseBuilder = new StringBuilder("[\"" + query + "\"");
         responseBuilder.append(",");
-        responseBuilder.append(autoCompleteService.getSuggests(query));
+        final List<String> suggestsAll = autoCompleteService.getSuggests(query);
+        final ArrayList<String> suggests = new ArrayList<>();
+        for (int i = 0; i < suggestsAll.size(); i++) {
+            suggests.add(suggestsAll.get(i));
+            if (i > 3) {
+                break;
+            }
+        }
+                      
+        suggests.add("\"Page d'aide\"");
+        responseBuilder.append(suggests.toString());
+        responseBuilder.append(",");
+        final ArrayList<String> descriptions = new ArrayList<>();
+        for (int i = 0; i < suggests.size(); i++) {
+            descriptions.add("\"test\"");
+            if (i > 3) {
+                break;
+            }
+        }
+        descriptions.add("\"AIDE\"");
+        responseBuilder.append(descriptions.toString());
+        responseBuilder.append(",");
+        final ArrayList<String> urls = new ArrayList<>();
+        for (int i = 0; i < suggests.size(); i++) {
+            urls.add("\"http://localhost/index.html\"");
+            if (i > 3) {
+                break;
+            }
+        }
+        urls.add("\"http://localhost/index.html\"");
+        responseBuilder.append(urls.toString());
         responseBuilder.append("]");
 
         return Response.status(200).entity(responseBuilder.toString()).build();
