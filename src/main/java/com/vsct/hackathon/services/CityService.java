abece86cc578a4a5c7e4d390c07e613b912472b1
Erwann Plouhinec <eplouhinec.prestataire@voyages-sncf.com>
2014-12-09 11:35:00 +0100

    Classe CityService

---
+++ src/main/java/com/vsct/hackathon/services/CityService.java
@@ -0,0 +1,18 @@
+package com.vsct.hackathon.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by erwann_plouhinec on 09/12/2014.
+ */
+public class CityService {
+
+    public List<String> getOrigine(String origineSaisie) {
+        return new ArrayList<String>();
+    }
+
+    public List<String> getDestination(String destinationSaisie) {
+        return new ArrayList<String>();
+    }
+}
